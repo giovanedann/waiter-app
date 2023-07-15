67419aa60070350b71c1cb06c6ed3d0c44089eb2
Giovane <giovanedaniell@gmail.com>
2023-07-15 19:09:19 -0300

    feat: serve images on /upload/:imageFileName endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,7 @@
+import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
+
 import { router } from './router';
 
 mongoose.connect('mongodb://localhost:27017')
@@ -7,6 +9,7 @@ mongoose.connect('mongodb://localhost:27017')
     const app = express();
     const PORT = 3001;
 
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
 
