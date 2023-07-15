2f9ec590543cab6c7b711219d082e78c36827916
Giovane <giovanedaniell@gmail.com>
2023-07-15 13:52:41 -0300

    [API] chore: establish connection to mongodb on server init

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-const PORT = 3001;
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => console.log('error connecting to MongoDB'));
+
