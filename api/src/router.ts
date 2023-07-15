152fb34bb5852426607cdaf451b856bab168b9a3
Giovane <giovanedaniell@gmail.com>
2023-07-15 14:15:00 -0300

    [API] feat: define application routes

---
+++ api/src/router.ts
@@ -0,0 +1,48 @@
+import { Router } from 'express';
+
+export const router = Router();
+
+// List categories
+router.get('/categories', (request, response) => {
+  response.send('List categories');
+});
+
+// Create category
+router.post('/categories', (request, response) => {
+  response.send('Create category');
+});
+
+// List products
+router.get('/products', (request, response) => {
+  response.send('List products');
+});
+
+// Create product
+router.post('/products', (request, response) => {
+  response.send('Create product');
+});
+
+// Get products by category
+router.get('/categories/:categoryId/products', (request, response) => {
+  response.send('Get products by category');
+});
+
+// List orders
+router.get('/orders', (request, response) => {
+  response.send('List orders');
+});
+
+// Create order
+router.post('/orders', (request, response) => {
+  response.send('Create order');
+});
+
+// Change order status
+router.patch('/orders/:orderId', (request, response) => {
+  response.send('Change order status');
+});
+
+// Delete/cancel order
+router.delete('/orders/:orderId', (request, response) => {
+  response.send('Delete/cancel order');
+});
