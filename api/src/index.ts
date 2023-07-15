51485786fd11cec81016145fd515660f647c32b3
Giovane <giovanedaniell@gmail.com>
2023-07-15 11:36:40 -0300

    [API] chore: add nodemon and ts-node

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3001, () => {
-  console.log('🚀 Server running on http://localhost:3001');
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
