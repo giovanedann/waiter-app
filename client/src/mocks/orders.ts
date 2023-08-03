b03feead4a0e1f5e3c98987cf23c316a53eb1578
Giovane <giovanedaniell@gmail.com>
2023-08-03 13:26:56 -0300

    fix(mocks): add missing image path file extension

--- client/src/mocks/orders.ts
+++ client/src/mocks/orders.ts
@@ -2,14 +2,14 @@ import { Order } from '../types/Order';
 
 export const ordersMock: Order[] = [
   {
-    _id: '6372e48cbcd195b0d3d0f7f3',
-    table: '123',
+    _id: '6372e48cbcd195b0d3d0f7v3',
+    table: '2',
     status: 'WAITING',
     products: [
       {
         product: {
           name: 'Pizza quatro queijos',
-          imagePath: '1668472896991-quatro-queijos.png',
+          imagePath: '1689457645644-quatro-queijos.png',
           price: 40,
         },
         quantity: 3,
@@ -18,7 +18,32 @@ export const ordersMock: Order[] = [
       {
         product: {
           name: 'Coca cola',
-          imagePath: '1668473462705-coca-cola.png',
+          imagePath: '1689458835868-coca-cola.png',
+          price: 7,
+        },
+        quantity: 2,
+        _id: '6372e48cbcd195b0d3d0f7f5'
+      }
+    ],
+  },
+  {
+    _id: '6372e48cbcd195b0d3d0f7f3',
+    table: '4',
+    status: 'DONE',
+    products: [
+      {
+        product: {
+          name: 'Pizza quatro queijos',
+          imagePath: '1689457645644-quatro-queijos.png',
+          price: 40,
+        },
+        quantity: 3,
+        _id: '6372e48cbcd195b0d3d0f7f4'
+      },
+      {
+        product: {
+          name: 'Coca cola',
+          imagePath: '1689458835868-coca-cola.png',
           price: 7,
         },
         quantity: 2,
