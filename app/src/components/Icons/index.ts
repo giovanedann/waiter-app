501fa3609b09652fb73a0e27a70b2a147b4fd293
Giovane <giovanedaniell@gmail.com>
2023-08-06 08:38:25 -0300

    [APP] chore: add index export file to components and icons

---
+++ app/src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+export { CheckCircle } from './CheckCircle';
+export { MinusCircle } from './MinusCircle';
+export { Close } from './Close';
+export { PlusCircle } from './PlusCircle';
+export { Empty } from './Empty/index';
