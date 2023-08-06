afbef496f408ce24d150538f65f19e6666ed966b
Giovane <giovanedaniell@gmail.com>
2023-08-06 09:26:35 -0300

    feat: add home screen containers to Main component

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,10 +1,26 @@
-import { Header } from '../components/Header';
+import { Header, Menu } from '../components';
+import { Categories } from '../components/Categories';
 import * as S from './styles';
 
 export function Main() {
   return (
-    <S.Container>
-      <Header />
-    </S.Container>
+    <>
+      <S.Container>
+        <Header />
+        <S.CategoriesContainer>
+          <Categories />
+        </S.CategoriesContainer>
+
+        <S.MenuContainer>
+          <Menu />
+        </S.MenuContainer>
+      </S.Container>
+
+      <S.Footer>
+        <S.FooterContainer>
+
+        </S.FooterContainer>
+      </S.Footer>
+    </>
   );
 }
