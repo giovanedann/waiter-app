e939659cc2215ebaa04786c78800b909b8d1d424
Giovane <giovanedaniell@gmail.com>
2023-08-06 09:25:51 -0300

    feat(components): add Categories component

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -1,3 +1,42 @@
+import { FlatList } from 'react-native';
+import { categories } from '../../mocks/categories';
+import { Text } from '../Text';
+import * as S from './styles';
+import { useState } from 'react';
+
 export function Categories() {
-  return null;
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+
+    setSelectedCategory(category);
+  }
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      data={categories}
+      contentContainerStyle={{ paddingRight: 24 }}
+      keyExtractor={(category) => category._id}
+      renderItem={({ item }) => {
+        const isSelected = item._id === selectedCategory;
+
+        return (
+          <S.Category key={item._id} onPress={() => handleSelectCategory(item._id)}>
+            <S.Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>
+                {item.icon}
+              </Text>
+            </S.Icon>
+
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
+              {item.name}
+            </Text>
+          </S.Category>
+        );
+      }}
+    />
+  );
 }
