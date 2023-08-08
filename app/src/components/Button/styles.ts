7e75acb581f7d6b5de5f760999fad160b91bef38
Giovane <giovanedaniell@gmail.com>
2023-08-08 00:41:44 -0300

    [APP] feat: add loading state on button

--- app/src/components/Button/styles.ts
+++ app/src/components/Button/styles.ts
@@ -2,6 +2,7 @@ import { styled } from 'styled-components/native';
 
 export const Container = styled.Pressable`
   background: ${({ disabled }) => disabled ? '#999' : '#d73035'};
+  min-width: 150px;
   border-radius: 48px;
   padding: 14px 24px;
   align-items: center;
