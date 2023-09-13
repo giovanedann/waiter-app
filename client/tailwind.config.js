2a3d387eb51d4512d22e376c04df2518f9494b08
Giovane <giovanedaniell@gmail.com>
2023-09-13 12:44:05 -0300

    feat: add GeneralSans font

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      'gen-sans': 'GeneralSans, Arial, sans-serif'
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
